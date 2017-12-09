b9c87d0b5621c67c65e6169213375e5682f1b549
taoxuewen <t15609600799@163.com>
2017-12-09 21:56:23 +0800

    simple GUI by ZHN and tao 1209

--- main.cpp
+++ main.cpp
@@ -4,50 +4,61 @@
 #include "resource.h"
 #include "BControl.h"
 
+const char* ImageMotor[8]={"请想象右手","请想象左手","请想象左右手","请想象左手右脚","请想象右手左脚"};
+
+bool imaging_flag=true;
+bool stop_flag=true;
+
 CBForm form(ID_Dialog);
 //change
 void cmd_cancel();
-/*
-class Th: public CThreading
+void cmd_start();
+void Form_resize()
 {
-public:
-	Th(){};
-	DWORD WINAPI run(LPVOID lpParameter)
-	{
-		int N=1;
-		while (N++<10)
-		{
-			form.Control(ID_label).TextSet(N);
-			Sleep(5);
-		}
-	}
-};
+	form.Control(ID_motor).Move((form.ClientWidth()-form.Control(ID_motor).Width())/2,(form.ClientHeight()-form.Control(ID_motor).Height())/2);
+}
 
-Th th1;
-*/
+
 
 DWORD WINAPI run(LPVOID lpParameter)
 {
-	int N=1;
-	while (N++<10)
+	static int N=0;
+	stop_flag=true;
+	while (imaging_flag)
 	{
-		form.Control(ID_label).TextSet(N);
-		Sleep(1000);
+		form.Control(ID_motor).TextSet("休息");
+		Sleep(5000);
+		form.Control(ID_motor).TextSet(ImageMotor[N]);
+		Sleep(5000);
+		if (N++>3)N=0;
 	}
+	stop_flag=false;
 	return 1;
 }
 
 main()
 {
-	form.BackColorSet(RGB(255,255,255));
-	form.Control(ID_BtCANCEL).BackColorSet(RGB(0,255,0));
+	//form.BackColorSet();
+	form.Control(ID_Btstart).BackColorSet(RGB(0,255,0));
 	form.Control(ID_label).TextSet(100);
+	form.Control(ID_motor).FontSizeSet(30);
 	form.Show();
-	form.EventAdd(ID_BtCANCEL,eCommandButton_Click,cmd_cancel);
+	form.EventAdd(ID_Btstart,eCommandButton_Click,cmd_start);
+	form.EventAdd(ID_end,eCommandButton_Click,cmd_cancel);
+	form.EventAdd(0,eForm_Resize,Form_resize);
 	return 0;
 }
-
+void cmd_start()
+{
+	imaging_flag=true;
+	form.Control(ID_flag).TextSet("");
+	HANDLE thread_t=CreateThread(NULL,0,run,NULL,0,NULL);
+	CloseHandle(thread_t);
+	
+}
 void cmd_cancel()
 {
-	HANDLE thread_t=CreateThread(NULL,0,run,NULL,0,NULL);
+	imaging_flag=false;
+	//while(stop_flag);
+	form.Control(ID_flag).TextSet("已结束");
 }
--- resource.h
+++ resource.h
@@ -5,15 +5,21 @@
 #define ID_Dialog                       101
 #define ID_BtOK                         1000
 #define ID_BtCANCEL                     1001
+#define ID_Btstart                      1001
 #define ID_label                        1002
+#define ID_Motornums                    1007
+#define ID_motor                        1008
+#define ID_end                          1009
+#define IDC_SPIN1                       1010
+#define ID_flag                         1011
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
